d4fd212559006ebf47ca6d26f98177483f1cadd1
hanong0321 <gksdmd123@gmail.com>
2024-09-21 21:26:29 +0900

    Test 1

--- Library.java
+++ Library.java
@@ -1,2 +1,57 @@
-public class Library {
+import java.awt.print.Book;
+import java.util.*;
+
+public class Library implements LibrarySystem{
+    private static final String address = "부산 남구 용소로 45";
+    private String name;
+    private List<Book> bookList;
+    private List<Users> userList;
+    private Book book;
+    private Users user;
+
+    @Override
+    public Optional<Book> findBook(String title) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Users> findUser(int userId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void addBook(Book book) {
+
+    }
+
+    @Override
+    public void addUser(Users User) {
+
+    }
+
+    @Override
+    public void returnBook(Book book) {
+
+    }
+
+    public static void main(String[] args) {
+
+        lhe
+        // pknuLibrary.users() // 유저 생성
 }
+
+class BookNotFoundException extends Exception{
+    //생성자
+    public BookNotFoundException(String msg){
+        super(msg);
+    }
+}
+
+class UserNotFoundException extends Exception{
+    //생성자
+    public UserNotFoundException(String msg){
+        super(msg);
+    }
+}
+
+
--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,3 +1,11 @@
-public interface LibrarySystem {
-    void print();
+import java.awt.print.Book;
+import java.util.Optional;
+
+interface LibrarySystem{
+    Optional<Book> findBook(String title);
+    Optional<Users> findUser(int userId);
+    void addBook(Book book);
+    void addUser(Users User);
+    void returnBook(Book book);
+
 }
